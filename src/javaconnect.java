06f60c4485fcd81eb7773ffe2c3acd8fb8e97862
priyachore <priya.chore1305@gmail.com>
2021-09-24 12:30:34 +0530

    first commit

---
+++ src/javaconnect.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Priya
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class javaconnect {
+    Connection conn=null;
+    
+    public static Connection ConnecrDb()
+    {
+        try
+        {
+           Class.forName("com.mysql.jdbc.Driver");
+          Connection conn=DriverManager.getConnection("jdbc:mysql://localhost/bank","root","");
+          return conn;
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
+    
+}
